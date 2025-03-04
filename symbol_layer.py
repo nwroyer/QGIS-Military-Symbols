240f6b9b246a83d6ccd12190835d02b39b32a88a
Nicholas Royer <nick.w.royer@gmail.com>
2025-03-04 15:07:40 -0500

    Fixed spacing

--- symbol_layer.py
+++ symbol_layer.py
@@ -3,29 +3,29 @@ from qgis.PyQt.QtGui import QColor
 
 class MilitarySymbolLayer(QgsMarkerSymbolLayer):
 
-  def __init__(self, radius=4.0):
-      QgsMarkerSymbolLayer.__init__(self)
-      self.radius = radius
+    def __init__(self, radius=4.0):
+        QgsMarkerSymbolLayer.__init__(self)
+        self.radius = radius
 
-  def layerType(self):
-     return "MilitarySymbolMarker"
+    def layerType(self):
+        return "MilitarySymbolMarker"
 
-  def properties(self):
-      return { "radius" : str(self.radius) }
+    def properties(self):
+        return { "radius" : str(self.radius) }
 
-  def startRender(self, context):
-    pass
+    def startRender(self, context):
+        pass
 
-  def stopRender(self, context):
-      pass
+    def stopRender(self, context):
+        pass
 
-  def renderPoint(self, point, context):
-      # Rendering depends on whether the symbol is selected (QGIS >= 1.5)
+    def renderPoint(self, point, context):
+        # Rendering depends on whether the symbol is selected (QGIS >= 1.5)
 
-      color = context.selectionColor() if context.selected() else QColor(255, 255, 0)
-      p = context.renderContext().painter()
-      p.setPen(color)
-      p.drawEllipse(point, self.radius, self.radius)
+        color = context.selectionColor() if context.selected() else QColor(255, 255, 0)
+        p = context.renderContext().painter()
+        p.setPen(color)
+        p.drawEllipse(point, self.radius, self.radius)
 
-  def clone(self):
-      return MilitarySymbolLayer(self.radius)
+    def clone(self):
+        return MilitarySymbolLayer(self.radius)
